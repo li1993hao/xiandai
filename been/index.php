4925c5b76dcfdd35055b6c17eec44b3a932a0476
1165116800 <1165116800@qq.com>
2014-10-12 16:24:24 +0800

    app_hjy

--- been/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('HTTP/1.1 404 Not Found');
-header("status: 404 Not Found");
-echo "404 No Found!";
-?>
--- setting/editor.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-*  Create On 2010-8-24
-*  Author Been
-*  QQ:281443751
-*  Email:binbin1129@126.com
-**/
-
-$EditorUploadBaseUrl = "http://localhost/wyjob/common/";
